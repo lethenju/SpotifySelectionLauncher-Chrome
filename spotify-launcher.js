302c6cac5bea5847e29dca9c453e1ba2ad430e35
Julien LE THENO <julien.letheno@gmail.Com>
2020-06-01 15:23:18 +0200

    [Cleanup] Add license and clean code

--- spotify-launcher.js
+++ spotify-launcher.js
@@ -1,8 +1,29 @@
+/*
+MIT License
 
+Copyright (c) 2020 Julien LE THENO
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 let spotify_tab_id;
 
 function store_tab_id(tab) {
-  console.log("storing id "+tab.id)
   spotify_tab_id = tab.id;
 }
 
@@ -10,14 +31,11 @@ chrome.contextMenus.create({
   title: "Play '%s' on Spotify", 
   contexts:["selection"], 
   onclick: function(info, tab) {
-    console.log(info.selectionText);
     const url = "https://open.spotify.com/search/"+info.selectionText;
     if (typeof spotify_tab_id !== 'undefined') {
       chrome.tabs.update(spotify_tab_id, {url, active:true});
-      
     } else {
       chrome.tabs.create({url}, store_tab_id);
     }
   }   
-
 });
